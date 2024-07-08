0df510a6733870437d3558b226e6fe2aaec67dc2
QuarterJes <jjjjjjjjesang@gmail.com>
2024-07-08 15:24:31 +0100

    Finish exercise 1-18

--- ChapterOne/Section9/main.c
+++ ChapterOne/Section9/main.c
@@ -5,12 +5,13 @@
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
 void append(char to[], char from[], int a, int b);
+int remove_blanks(char to[], int maxline);
 
 main() {
   // exercise 1-16
-  char longest_line[MAXLENGTH];
-  char line[MAXLENGTH];
-  int longest, current;
+  // char longest_line[MAXLENGTH];
+  // char line[MAXLENGTH];
+  // int longest, current;
 
   // longest = 0;
   // while ((current = get_line(line, MAXLENGTH)) > 0) {
@@ -23,25 +24,45 @@ main() {
   // printf("\n%s \n", longest_line);
 
   // exercise 1-17
-  char above_threshold[MAXLENGTH]; 
-  int total_characters, total_lines;
-  total_characters = total_lines = 0;
-  
-  while ((current = get_line(line, MAXLENGTH)) > 0) {
-    if (current-1 > THRESHOLD) {
-      ++total_lines;
-      total_characters += current;
-      if (total_lines == 1)
-        copy(above_threshold, line);
+  //  char above_threshold[MAXLENGTH]; 
+  //  int total_characters, total_lines;
+  //  total_characters = total_lines = 0;
+  //  
+  //  while ((current = get_line(line, MAXLENGTH)) > 0) {
+  //    if (current-1 > THRESHOLD) {
+  //      ++total_lines;
+  //      total_characters += current;
+  //      if (total_lines == 1)
+  //        copy(above_threshold, line);
+  //      else
+  //        append(above_threshold, line, total_characters - current, total_characters);
+  //    }
+  //  }
+ 
+  //  if (total_lines > 0)
+  //    printf("\n%s \n", above_threshold);
+  //
+  //
+ 
+  // exercise 1-18
+
+  char current_line[MAXLENGTH], output[MAXLENGTH];
+  int line_characters, total_characters;  
+  line_characters = total_characters = 0;
+ 
+  while ((line_characters = remove_blanks(current_line, MAXLENGTH)) > 0) {
+    if (line_characters > 1) {
+      total_characters += line_characters;
+      if (total_characters - line_characters == 0)
+        copy(output, current_line);
       else
-        append(above_threshold, line, total_characters - current, total_characters);
+        append(output, current_line, total_characters - line_characters, total_characters);
     }
   }
 
-  if (total_lines > 0)
-    printf("\n%s \n", above_threshold);
-
-  }
+  if (total_characters > 0)
+    printf("%s \n", output);
+}
   
   
 int get_line(char line[], int maxline) {
@@ -80,4 +101,38 @@ void append(char to[], char from[], int start, int end) {
   }
 
   to[end-1] = '\0';
+}
+
+int remove_blanks(char line[], int maxline) {
+  int i, j, spaces, tabs;
+  char c;
+
+  spaces = tabs = 0;
+  i = 0;
+
+  while ((c = getchar()) != EOF && c != '\n') {
+    if (c == ' ') {
+      ++spaces;
+      if (spaces < 2) {
+        line[i] = c;
+        ++i;
+      }
+    }
+    else if (c == '\t') {
+      ++tabs;
+      if (tabs < 2) {
+        line[i] = c;
+        ++i;
+      }
+    }
+    else {
+      spaces = tabs = 0;
+      line[i] = c;
+      ++i;
+    }
+  }
+  if (c == '\n')
+    ++i;
+
+  return i;
 }
