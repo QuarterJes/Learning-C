6bd984d33395390e14193f333be018aface3ec72
QuarterJes <jjjjjjjjesang@gmail.com>
2024-10-30 02:54:47 +0000

    First test commit on T480

--- ChapterTwo/2-1/main.c
+++ ChapterTwo/2-1/main.c
@@ -3,18 +3,18 @@
 #include <float.h>
 
 int main(int argc, char **argv) {
-  // for char
+  /* for char */
   printf("========== char ==========\n");
   printf("Value for char: %d\n", CHAR_BIT);
 
-  // for short int
+  /* for short int */
   printf("\n========== short int ==========\n");
   printf("Minimum value for a short int: %d\n", SHRT_MIN);
   printf("Maximum value for a short int: %d\n", SHRT_MAX);
   printf("Minimum value for an unsigned short int: 0\n");
   printf("Maximum value for an unsigned short int: %d\n", USHRT_MAX);
 
-  // for int
+  /* for int */
   printf("\n========== int ==========\n");
   printf("Minimum value for int: %d\n", INT_MIN);
   printf("Maximum value for int: %d\n", INT_MAX);
@@ -22,13 +22,14 @@ int main(int argc, char **argv) {
   printf("Maximum value for an unsigned int: %u\n", UINT_MAX);
 
 
-  // for long int
+  /* for long int */
   printf("\n========== long int ==========\n");
   printf("Minmum value for long int: %ld\n", LONG_MIN);
   printf("Maximum value for long int: %ld\n", LONG_MAX);
   printf("Minimum value for unsigned long int: 0");
   printf("Maximum value for unsigned long int: %lu\n", ULONG_MAX);
 
-  // for short float
-  // TODO
+  /*for short float
+  TODO
+  */
 }
