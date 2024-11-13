ff43f20d735a72e0d809288d8a8b26491c99ed05
QuarterJes <QuarterJes@gmail.com>
2024-11-13 09:37:25 +0000

    Do 2-1 and 2-2

--- ChapterTwo/2-2/main.c
+++ ChapterTwo/2-2/main.c
@@ -1,5 +1,30 @@
-/* test */
+#include <stdio.h>
 
-int main(int argc, int **argv) {
+#define MAXLINE 99
+
+int getline(char to[], int lim);
+
+int main(int argc, char **argv) {
+  char s[MAXLINE];
+  int result = getline(s, MAXLINE);
+  printf("%d\n", result);
   return 0;
+}
+
+int getline(char s[], int lim) {
+  int valid = 1;
+  int i = 0;
+  while (valid) {
+    int c = getchar();
+    if (c == EOF)
+      valid = 0;
+    else if (c == '\n')
+      valid = 0;
+    else {
+      s[i] = c;
+      ++i;
+    }
+  }
+  s[i] = '\0';
+  return i;
 }
